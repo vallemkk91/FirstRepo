5c31193c4d4e152265febb65e9b61d5c019335d7
vallemkk91@gmail.com <vallemkk91@gmail.com>
2024-04-06 22:25:19 +0530

    graphQL fourth commit

--- src/test/java/gitPractice/gitHelloWorld.java
+++ src/test/java/gitPractice/gitHelloWorld.java
@@ -4,6 +4,10 @@ public class gitHelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		int a=2;
+		int b=5;
+		int c= a+b;
+		System.out.println(c);
 
 	}
 
